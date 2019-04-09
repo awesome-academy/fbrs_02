b4c0ba88505e39df1338da6d6433905b0d32f197
BlackPearl96 <van9665@gmail.com>
2019-04-09 12:38:12 +0700

    book_list_like

--- app/controllers/suggests_controller.rb
+++ app/controllers/suggests_controller.rb
@@ -2,7 +2,7 @@ class SuggestsController < ApplicationController
   before_action :logged_in_user
   before_action :load_suggest, only: :destroy
   before_action :suggest_by_user, only: :index
-  load_and_authorize_resource
+  authorize_resource
 
   def index; end
 
